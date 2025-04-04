e32df1ab7274635154ee21c2993f111d1e7329b5
MarcinDudek <dudek.v@gmail.com>
2025-04-04 16:05:42 +0200

    add example demonstrating nil slices and nil maps in Go

---
+++ gotchas/008_nil_slice_nil_map.go
@@ -0,0 +1,12 @@
+package gotchas
+
+func nilSlice() {
+	var s []int // nil slice
+
+	s = append(s, 1) // it is on to add items to "nil" slice
+}
+
+func nilMap() {
+	var m map[string]int
+	m["one"] = 1 // error
+}
