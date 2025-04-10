4c2cfc4d1b8ba38e6543a4f63a534dcad0b0a043
MarcinDudek <dudek.v@gmail.com>
2025-04-10 08:56:47 +0200

    add Display method test for LinkedList

--- singly_linked_list/singly_linked_list_test.go
+++ singly_linked_list/singly_linked_list_test.go
@@ -16,3 +16,12 @@ func TestToSlice(t *testing.T) {
 
 	assert.Equal(t, []int{1, 2, 3}, result)
 }
+
+func TestDisplay(t *testing.T) {
+	list := &LinkedList{}
+	list.Insert(1)
+	list.Insert(2)
+	list.Insert(3)
+
+	list.Display()
+}
