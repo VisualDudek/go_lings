fea5f74c8ef95c1fb9ef9885b06d9ea86d8023ab
MarcinDudek <dudek.v@gmail.com>
2025-04-14 11:23:54 +0200

    add example demonstrating blocking behavior when sending to a channel

---
+++ gotchas/029_sending_to_close_chan_panic_TMP/sending_to_close_chan_panic.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan int)
+
+	for i := range 3 {
+		go func(idx int) {
+			fmt.Println("sending to ch: ", (idx+1)*2)
+			ch <- (idx + 1) * 2 // TO JEST BLOCKING; SENDER is blocking
+			fmt.Println("sent to ch: ", (idx+1)*2)
+		}(i)
+	}
+
+	// get first result
+	fmt.Println(<-ch)
+	// close(ch) // not ok (you still have other senders)
+	time.Sleep(time.Second * 3)
+	// checking if sender is blocked ??? should not be, A JEDNAK JEST
+	fmt.Println(<-ch)
+	fmt.Println(<-ch)
+	time.Sleep(time.Second * 1)
+}
