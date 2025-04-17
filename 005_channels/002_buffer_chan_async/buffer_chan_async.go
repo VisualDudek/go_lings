3f242e43de8265ed4d9dc1f36e693200e59ab8fc
MarcinDudek <dudek.v@gmail.com>
2025-04-17 12:12:42 +0200

    fix: add timestamps to log messages in producer and consumer functions

--- 005_channels/002_buffer_chan_async/buffer_chan_async.go
+++ 005_channels/002_buffer_chan_async/buffer_chan_async.go
@@ -10,22 +10,22 @@ import (
 func producer(ch chan string) {
 	for i := 1; i <= 6; i++ {
 		msg := fmt.Sprintf("Message %d", i)
-		fmt.Println("Sending:", msg)
+		fmt.Println(time.Now().Format("15:04:05.0000000"), "Sending:", msg)
 		ch <- msg // this won't block unless the buffer is full
-		fmt.Println("Sent:", msg)
+		fmt.Println(time.Now().Format("15:04:05.0000000"), "Sent:", msg)
 	}
 	close(ch)
 }
 
 func consumer(ch chan string) {
 	for msg := range ch {
-		fmt.Println("Received:", msg)
+		fmt.Println(time.Now().Format("15:04:05.0000000"), "Received:", msg)
 		time.Sleep(time.Second * 2) // simulate slow processing
 	}
 }
 
 func main() {
-	ch := make(chan string, 1) // buffered channel with capacity 2
+	ch := make(chan string, 4) // buffered channel with capacity 2
 	fmt.Println("Capacity of ch: ", cap(ch))
 
 	go producer(ch)
