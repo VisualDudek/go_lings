c2d6432e4cbe8efe7b692f77b2d6ba9b1b39e2e1
MarcinDudek <dudek.v@gmail.com>
2025-04-17 10:36:08 +0200

    feat: add custom labeled benchmark for myFunc

--- 007_benchmark/benchmark_test.go
+++ 007_benchmark/benchmark_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
@@ -15,4 +16,15 @@ func BenchmarkMyFunc(b *testing.B) {
 		myFunc()
 	}
 
+}
+
+func BenchmarkMyFuncCustomLabels(b *testing.B) {
+	for _, size := range []int{10, 100, 1_000} {
+		b.Run(fmt.Sprintf("Size%d", size), func(b *testing.B) {
+			for i := range b.N {
+				_ = i
+				myFunc()
+			}
+		})
+	}
 }
