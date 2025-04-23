f504b21e1453a7548415960c9cb7ebf5ebca7521
MarcinDudek <dudek.v@gmail.com>
2025-04-23 09:32:27 +0200

    feat: add initial implementation of Item struct and main function for slice demonstration

---
+++ 002_slices/004_literals_struct/main.go
@@ -0,0 +1,12 @@
+package main
+
+type Item struct {
+	ID    int
+	Ready bool
+}
+
+func main() {
+	// create slice of struct literal with int and bool fields
+	// provide two solution first with predefined Item struct and
+	// second with plain struct
+}
