df4ae6ef15fd53822c9ee634b70a285e4b41f523
MarcinDudek <dudek.v@gmail.com>
2025-04-23 09:53:54 +0200

    feat: add make and append demonstration with comments in new file

--- 002_slices/003_pointer/main.go
+++ 002_slices/003_pointer/main.go
@@ -3,5 +3,6 @@ package main
 func main() {
 	s := []int{1, 2, 3}
 	// Print slice header AND memory address that a slice points to
+	_ = s
 
 }
---
+++ 002_slices/006_make_and_append/make_and_append.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	a := make([]int, 5)
+
+	a = append(a, 1)
+	// co miałeś na myśli a co się stało ?
+	// jak to naprawić ?
+	_ = a
+
+}
