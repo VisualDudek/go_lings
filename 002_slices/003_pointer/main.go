82bee8964195712767bf184b6ab524a67b240fff
MarcinDudek <dudek.v@gmail.com>
2025-04-25 09:40:31 +0200

    add verbose comment

--- 002_slices/003_pointer/main.go
+++ 002_slices/003_pointer/main.go
@@ -3,6 +3,8 @@ package main
 func main() {
 	s := []int{1, 2, 3}
 	// Print slice header AND memory address that a slice points to
+	// in other words usig format secifiers get addres of slice header and
+	// adress of first element in slice
 	_ = s
 
 }
