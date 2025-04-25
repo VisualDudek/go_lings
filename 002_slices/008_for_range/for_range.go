a2781646212efa4b239d6861fb006a589e48be8f
MarcinDudek <dudek.v@gmail.com>
2025-04-25 11:41:43 +0200

    add missing assignment to variable in main function

--- 002_slices/008_for_range/for_range.go
+++ 002_slices/008_for_range/for_range.go
@@ -5,6 +5,7 @@ var s = []int{1, 2, 3, 4, 5}
 // ważne jest żeby pamiętać że value jest copią a nie referencją !!!
 
 func main() {
+	_ = s
 	// iteruj po idx i v
 
 	// iteruj tylko po idx (może powodować błędy w logice jak pomylisz z v)
