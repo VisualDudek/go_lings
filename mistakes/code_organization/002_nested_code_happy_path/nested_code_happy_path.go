df94eb66b883e5f171f71ec8b0ee054c82faabc3
MarcinDudek <dudek.v@gmail.com>
2025-04-25 16:57:14 +0200

    happy path

---
+++ mistakes/code_organization/002_nested_code_happy_path/nested_code_happy_path.go
@@ -0,0 +1,50 @@
+package main
+
+import "errors"
+
+// refactor to single happy path
+
+func join(s1, s2 string, max int) (string, error) {
+	if s1 == "" {
+		return "", errors.New("s1 is empty")
+	} else {
+		if s2 == "" {
+			return "", errors.New("s2 is empty")
+		} else {
+			concat, err := concatenate(s1, s2)
+			if err != nil {
+				return "", err
+			} else {
+				if len(concat) > max {
+					return concat[:max], nil
+				} else {
+					return concat, nil
+				}
+			}
+		}
+	}
+}
+
+func concatenate(s1, s2 string) (string, error) {
+	// ...
+}
+
+// --- Solution ---
+
+func joinHappyPath(s1, s2 string, max int) (string, error) {
+
+	if s1 == "" {
+		return "", errors.New("s1 is empty")
+	}
+	if s2 == "" {
+		return "", errors.New("s2 is empty")
+	}
+	concat, err := concatenate(s1, s2)
+	if err != nil {
+		return "", err
+	}
+	if len(concat) > max {
+		return concat[:max], nil
+	}
+	return concat, nil
+}
