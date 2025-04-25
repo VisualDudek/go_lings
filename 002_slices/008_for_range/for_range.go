5db0601a703180aec01a2d1d0383148656956e6a
MarcinDudek <dudek.v@gmail.com>
2025-04-25 11:40:37 +0200

    add slices for range

---
+++ 002_slices/008_for_range/for_range.go
@@ -0,0 +1,14 @@
+package main
+
+var s = []int{1, 2, 3, 4, 5}
+
+// ważne jest żeby pamiętać że value jest copią a nie referencją !!!
+
+func main() {
+	// iteruj po idx i v
+
+	// iteruj tylko po idx (może powodować błędy w logice jak pomylisz z v)
+
+	// iteruj tylko po value
+
+}
