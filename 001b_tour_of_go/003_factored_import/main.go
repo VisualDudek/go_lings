8a60a569f3377524c66ef1667b8a655defb13aaf
MarcinDudek <dudek.v@gmail.com>
2026-01-23 13:58:39 +0100

    add: create main package with factored imports and example usage

---
+++ 001b_tour_of_go/003_factored_import/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// imports can be factored into a block like this
+/*
+import (
+	"fmt"
+	"math"
+)
+*/
+
+func main() {
+	fmt.Println("Welcome to the Tour of Go!")
+	fmt.Println("The square root of 16 is:", math.Sqrt(16))
+}
