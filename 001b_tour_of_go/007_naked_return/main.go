ebcc16ecf0384d9f5b5d7b15f2fe1d623590a7f3
MarcinDudek <dudek.v@gmail.com>
2026-01-23 14:40:09 +0100

    docs: Add example demonstrating naked returns with named return values

---
+++ 001b_tour_of_go/007_naked_return/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// TAKE: Named return values can be used to simplify return statements.
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+func main() {
+	fmt.Println(split(17))
+}
