e1c0f1028fbc790c1818150ecd238de9b015ef73
MarcinDudek <dudek.v@gmail.com>
2026-01-23 13:58:44 +0100

    add: create main.go for exported names example with math.Pi

---
+++ 001b_tour_of_go/004_exported_names/main.go
@@ -0,0 +1,13 @@
+// TODO: Peek where math.Pi lives in the math package
+// Ctrl+Shift+F10 to peek
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Println("Welcome to the Tour of Go!")
+	fmt.Printf("The pi is %v\n", math.Pi) // math.pi lowercase 'p' is not exported
+}
