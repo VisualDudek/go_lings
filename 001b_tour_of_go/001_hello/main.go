a864b5fdbbece71c479cb5b4ece815314cc72293
MarcinDudek <dudek.v@gmail.com>
2026-01-23 11:49:05 +0100

    add: tour of go hello

---
+++ 001b_tour_of_go/001_hello/main.go
@@ -0,0 +1,12 @@
+// This program demonstrates the basic structure of a Go executable:
+// - package main declaration
+// - importing the fmt package
+// - func main() as the entry point
+// - using fmt.Println() to write output to standard output
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
